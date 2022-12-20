3fbee61419d8c20af53b355b9f4a3f8508bad743
ACJ <gitlab@acjs.net>
2022-12-20 10:16:32 +0100

    Turn it into a _CakePHP_ plugin
    Requires testing.

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin {
+
+    /**
+     * @var bool
+     */
+    protected $middlewareEnabled = false;
+
+    /**
+     * @var bool
+     */
+    protected $bootstrapEnabled = false;
+
+    /**
+     * @var bool
+     */
+    protected $routesEnabled = false;
+
+}
